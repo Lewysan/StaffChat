e81f255ea825199d7f9842d64d130c1803552d8d
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2022-04-19 01:47:02 -0700

    Update StellarCore.java

--- src/main/java/me/tyranzx/staffcore/StellarCore.java
+++ src/main/java/me/tyranzx/staffcore/StellarCore.java
@@ -2,19 +2,44 @@ package me.tyranzx.staffcore;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class StellarCore extends JavaPlugin implements CommandExecutor {
+import java.util.ArrayList;
 
-    String permiso = "permiso.permiso";
+public final class StellarCore extends JavaPlugin implements CommandExecutor, Listener {
 
+    private final String permiso = "permiso.permiso";
+    private final ArrayList<CommandSender> staffchat = new ArrayList<>();
+    private final Server server = Bukkit.getServer();
+
+    public String c(String msg){
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+    @EventHandler
+    public void staffchatActivated(AsyncPlayerChatEvent e) {
+        if (!staffchat.contains(e.getPlayer())) return;
+
+        e.setCancelled(true);
+        e.getRecipients().clear();
+
+        for (Player staff : server.getOnlinePlayers()) {
+            Bukkit.getConsoleSender().sendMessage(c("&8[&aStaffChat&8] &f")+e.getPlayer().getName()+c(": &b")+e.getMessage());
+            staff.sendMessage(c("&8[&aStaffChat&8] &f") + e.getPlayer().getName() + c(": &b") + e.getMessage());
+        }
+    }
     @Override
     public void onEnable() {
         this.getCommand("sc").setExecutor(this);
+        server.getPluginManager().registerEvents(this, this);
     }
     @Override
     public boolean onCommand(CommandSender p, Command command, String label, String[] args) {
@@ -24,26 +49,41 @@ public final class StellarCore extends JavaPlugin implements CommandExecutor {
         }
         if (command.getName().equalsIgnoreCase("sc")){
             if (args.length == 0){
-                p.sendMessage(c("&cDebes especificar un mensaje."));
-                return true;
-            }
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < args.length; i++) {
-                sb.append(args[i]);
-                if(i < args.length) {
-                    sb.append(" ");
+                if (!staffchat.contains(p)){
+                    if (p instanceof Player) {
+                        staffchat.add(p);
+                        p.sendMessage(c("&7Staffchat &aactivado"));
+                        return true;
+                    } else {
+                        p.sendMessage(c("&cNo puedes activar el modo staffchat por la consola."));
+                    }
+                } else {
+                    staffchat.remove(p); p.sendMessage(c("&7Staffchat &cdesactivado"));
                 }
             }
-            for (Player online : Bukkit.getServer().getOnlinePlayers()){
-                if (online.hasPermission(permiso)){
-                    online.sendMessage(c("&e[StaffChat] &c")+p.getName()+c(": &b")+sb.toString());
-                    return true;
+            if (args.length >= 1) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < args.length; i++) {
+                    sb.append(args[i]);
+                    if (i < args.length) {
+                        sb.append(" ");
+                    }
+                }
+                for (Player staff : server.getOnlinePlayers()){
+                    if (staff.hasPermission("essentials.staff")){
+                        if (p instanceof Player){
+                            Bukkit.getConsoleSender().sendMessage(c("&8[&aStaffChat&8] &f")+p.getName()+c(": &b")+sb.toString());
+                            staff.sendMessage(c("&8[&aStaffChat&8] &f")+p.getName()+c(": &b")+sb.toString());
+                        }
+                        else if (p instanceof ConsoleCommandSender){
+                            Bukkit.getConsoleSender().sendMessage(c("&8[&aStaffChat&8] &e")+p.getName()+c(": &b")+sb.toString());
+                            staff.sendMessage(c("&8[&aStaffChat&8] &e")+p.getName()+c(": &b")+sb.toString());
+                        }
+                        return true;
+                    }
                 }
             }
         }
         return false;
-    }
-    public String c(String msg){
-        return ChatColor.translateAlternateColorCodes('&', msg);
     }
 }
